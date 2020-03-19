2adc3129ddae5e659d989b2814be7827273b844e
Pablo Gonzalez <pabgon0@gmail.com>
2020-03-19 17:44:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,4 +6,25 @@ class School
     @roster = {}
   end
   
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    roster.each do |class_grade, student|
+      if class_grade == grade
+        return student
+      end
+    end
+  end
+  
+  def sort
+    sorted_list = {}
+    roster.each do |grade, student|
+      sorted_list[grade] = student.sort
+    end
+    sorted_list
+  end
+  
 end
